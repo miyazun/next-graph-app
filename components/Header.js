76b7b3a384a4c123dcf6954e32aaaf40f31e5979
miyazun <76639695+miyazun@users.noreply.github.com>
2023-02-15 03:31:21 +0900

    index.jsの内容を変更。ワイヤーフレームのヘッダーの部分を出力と設定(css&js)

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import styles from '@/styles/Home.module.css'
+
+export function Header(){
+    return (
+        <header className={styles.center}>
+            <a>Title</a>
+        </header>
+    );
+}
