00b4c9e04d05e02e45b5c914a347c6f868c34449
miyazun <76639695+miyazun@users.noreply.github.com>
2023-02-16 05:45:17 +0900

    ヘッダーをレスポンシブルデザインに対応できるようにした

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import styles from '@/styles/Home.module.css'
 
 export function Header(){
     return (
-        <header className={styles.center}>
+        <header className={styles}>
             <a>Title</a>
         </header>
     );
